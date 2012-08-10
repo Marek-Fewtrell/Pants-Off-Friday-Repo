dc4ad5ee1f74635b6c732c7ac04e6b1eedeedb9e
Marek Fewtrell <Marek.few@gmail.com>
2012-08-10 16:42:31 +1000

    Initial Blank Project
    Initial blank project created by James. Has nothing in it. Will be used
    for project.

---
+++ SolarPowerCalculatorProject/src/project/index.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package project;
+
+/**
+ * @author james
+ *
+ */
+public class index {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//TODO auto generated
+		//I don't know what this does - James
+		
+	}
+
+}

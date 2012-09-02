9b8d66b49ced540b7b4d3e21512d9fc8bd60b411
Marek Fewtrell <Marek.few@gmail.com>
2012-09-02 13:51:23 +1000

    Fixed WAR file errors. Fixed errors pertaining to the update to the app engine sdk (1.7.0-1.7.1). Just did a quick fix which synchronized the lib in war file to the other included libraries.
    Signed-off-by: Marek Fewtrell <Marek.few@gmail.com>

---
+++ SolarPowerCalculatorProject/test/project/test/RandTest.java
@@ -0,0 +1,13 @@
+package project.test;
+
+// *****
+//
+// THIS CLASS WE CREATED SO THE TEST FOLDER
+// WOULD BE IN THE GIT REPOSITORY AND THE FILE STRUCTURE
+// WAS COMPLETE AND FUNCTINOAL.
+//
+// *****
+
+public class RandTest {
+
+}

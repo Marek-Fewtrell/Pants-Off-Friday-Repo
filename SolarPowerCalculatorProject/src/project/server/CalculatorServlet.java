731709dd7b163054f7fe0725287bc7c5b1706927
ksichter <operation_sting@hotmail.com>
2012-09-04 19:06:27 +1000

    Update SolarPowerCalculatorProject/src/project/server/CalculatorServlet.java

--- SolarPowerCalculatorProject/src/project/server/CalculatorServlet.java
+++ SolarPowerCalculatorProject/src/project/server/CalculatorServlet.java
@@ -1,25 +1,37 @@
 package project.server;
 
 import java.io.IOException;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CalculatorServlet extends HttpServlet{
 	
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response){
 		String panel = request.getParameter("solarPanel");
-		int numPanels = Integer.parseInt(request.getParameter("numPanels"));
+		String numPanels = request.getParameter("numPanels");
 		String inverter = request.getParameter("inverter");
 		double daytimeUsage = Double.parseDouble(request.getParameter("daytimeUsage"));
 		double efficiencyLoss = Double.parseDouble(request.getParameter("efficiencyLoss"));
 		String postcode = request.getParameter("postcode");
 		String suburb = request.getParameter("suburb");
 		String energyProvider = request.getParameter("energyProvider");
+		HttpSession session = request.getSession(true);
+		session.setAttribute("solarPanel", request.getParameter("solarPanel"));
+		session.setAttribute("numPanels", request.getParameter("numPanels"));
+		session.setAttribute("inverter", request.getParameter("inverter"));
+		session.setAttribute("daytimeUsage", request.getParameter("daytimeUsage"));
+		session.setAttribute("efficiencyLoss", request.getParameter("efficiencyLoss"));
+		session.setAttribute("postcode", request.getParameter("postcode"));
+		session.setAttribute("suburb", request.getParameter("suburb"));
+		session.setAttribute("energyProvider", request.getParameter("energyProvider"));
 		try {
-			response.sendRedirect("contact us.jsp");
+			//response.sendRedirect("/contact us.jsp");
+			response.sendRedirect("/powerGeneratedReport.jsp");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

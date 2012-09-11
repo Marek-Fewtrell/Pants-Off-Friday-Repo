894196dfbc422b440e92eba43839631fae539ce2
James Whitehead <jwhitehead176@gmail.com>
2012-09-11 22:16:42 +1000

    Update SolarPowerCalculatorProject/src/project/test/CalculationsTest.java
    added test class method stubs in comment form
    added test on default constructor

--- SolarPowerCalculatorProject/src/project/test/CalculationsTest.java
+++ SolarPowerCalculatorProject/src/project/test/CalculationsTest.java
@@ -20,14 +20,45 @@ public class CalculationsTest {
 	private int orientation = 0;
 	
 	@Before
-	public void setup(){
+	public void setup() {
 		test = new Calculations(panelNumber, numPanels, suburb, inverterNumber,
 				energyCompany, dailyUsage, tilt, orientation);
 	}
 	
+	@After
+	public void teardown() {
+		test = null;
+	}
+	
+	// Test to ensure constructor working as expected
+	@Test
+	public void testCalculatorConstructor() {
+		assertTrue(panelNumber == "BLUE230/07");
+		assertTrue(numPanels == 16);
+		assertTrue(suburb == "BRISBANE");
+		assertTrue(inverterNumber == "SB3000");
+		assertTrue(energyCompany == "Click Energy");
+		assertTrue(dailyUsage == 10.0);
+		assertTrue(tilt == 20);
+		assertTrue(orientation == 0);
+	}
+
 	@Test
 	public void testGetDailyOutput() {
 		assertTrue("", test.getPowerGenerated(1) == 5);
 	}
 
+	// Test lower boundary limit on orientation at 100% efficiency (negative)
+
+	// Test upper boundary limit on orientation at 100% efficiency (positive)
+
+	// Test borderline lower limit on orientation at 100% efficiency (negative)
+
+	// Test borderline upper limit on orientation at 100% efficiency (positive)
+
+	// Test orientation at case 0 (exactly middle)
+
+	// Test case outside lower boundary limit at 100% efficiency (outsize maximum negative orientation)
+
+	// Test case outside upper boundary limit at 100% efficiency (outsize maximum positive orientation)
 }

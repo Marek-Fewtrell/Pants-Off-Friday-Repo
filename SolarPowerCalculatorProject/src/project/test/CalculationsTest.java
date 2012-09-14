bcce7bb979dbbf4a620dc42f286e08650b5c7326
ksichter <operation_sting@hotmail.com>
2012-09-14 16:11:21 +1000

    Update SolarPowerCalculatorProject/src/project/test/CalculationsTest.java

--- SolarPowerCalculatorProject/src/project/test/CalculationsTest.java
+++ SolarPowerCalculatorProject/src/project/test/CalculationsTest.java
@@ -1,53 +1,92 @@
 package project.test;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.utils.SystemProperty.Environment;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+
 import org.junit.After;
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import project.server.Calculations;
 
-public class CalculationsTest {
 
+public class CalculationsTest {
+	
 	private Calculations test;
-	private String panelNumber = "BLUE230/07";
+	private String panelNumber = "REC230PE";
 	private int numPanels = 16;
-	private String suburb = "BRISBANE";
-	private String inverterNumber = "SB3000";
-	private String energyCompany = "Click Energy";
+	private String suburb = "Brisbane";
+	private String inverterNumber = "SB1600TL";
+	private String energyCompany = "Origin";
 	private double dailyUsage = 10.0;
 	private int tilt = 20;
 	private int orientation = 0;
+	public static final String decFormat = "#.##";
+	DecimalFormat decForm = new DecimalFormat(decFormat);
 	
 	@Before
 	public void setup() {
+		//helper.setUp();
 		test = new Calculations(panelNumber, numPanels, suburb, inverterNumber,
-				energyCompany, dailyUsage, tilt, orientation);
+				energyCompany, dailyUsage, tilt, orientation, "test");
 	}
 	
 	@After
 	public void teardown() {
+		//helper.tearDown();
 		test = null;
 	}
-	
+		
 	// Test to ensure constructor working as expected
 	@Test
 	public void testCalculatorConstructor() {
-		assertTrue(panelNumber == "BLUE230/07");
-		assertTrue(numPanels == 16);
-		assertTrue(suburb == "BRISBANE");
-		assertTrue(inverterNumber == "SB3000");
-		assertTrue(energyCompany == "Click Energy");
-		assertTrue(dailyUsage == 10.0);
-		assertTrue(tilt == 20);
-		assertTrue(orientation == 0);
+		assertTrue(test.getPanelNumber() == "REC230PE");
+		assertTrue(test.getNumberPanels() == 16);
+		assertTrue(test.getInverterNumber() == "SB1600TL");
+		assertTrue(test.getEnergyCompany() == "Origin");
+		assertTrue(test.getSystemPower() == 1.7);
+		assertTrue(test.getTariff11() == 0.253781);
+		assertTrue(test.getSolarExposure() == 5.3);
+		assertTrue(test.getLatitude() == 27);
+		assertTrue(test.getInverterEfficiency() == 0.96);
+		assertTrue(test.getReplacementGeneration() == 10.0);
+		assertTrue(test.getTilt() == 20);
+		assertTrue(test.getOrientation() == 0);
+		System.out.print(test.getPowerGenerated(1));
+		System.out.print(test.getPowerGenerated(10));
+		System.out.print(test.getDailySavings(1));
 	}
 
 	@Test
-	public void testGetDailyOutput() {
-		assertTrue("", test.getPowerGenerated(1) == 5);
+	public void testGetDailyOutputYearOne() {
+		assertTrue(decForm.format(test.getPowerGenerated(1)).equals("8.65"));
+	}
+	
+	@Test
+	public void testGetDailyOutputYearTen() {
+		assertTrue(decForm.format(test.getPowerGenerated(10)).equals("8.1"));
+	}
+	
+	@Test
+	public void testGetDailySavingsYearOne(){
+		assertTrue(decForm.format(test.getDailySavings(1)).equals("2.2"));
 	}
 
+	@Test
+	public void testGetDailySavingsYearTen(){
+		assertTrue(decForm.format(test.getDailySavings(10)).equals("2.06"));
+	}
 	// Test lower boundary limit on orientation at 100% efficiency (negative)
 
 	// Test upper boundary limit on orientation at 100% efficiency (positive)
